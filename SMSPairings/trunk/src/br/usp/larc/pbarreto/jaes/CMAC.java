277b275b6facfe06007b0738d05f53a65b6fc1b4
geovandro.pereira <geovandro.pereira@f51e17c9-8b49-0410-878b-2ba91e509939>
2008-12-09 03:35:56 +0000

    Correção no CMAC, init correto

--- SMSPairings/trunk/src/br/usp/larc/pbarreto/jaes/CMAC.java
+++ SMSPairings/trunk/src/br/usp/larc/pbarreto/jaes/CMAC.java
@@ -1,5 +1,4 @@
 package br.usp.larc.pbarreto.jaes;
-
 /*
  * CMAC.java
  *
@@ -22,13 +21,12 @@ package br.usp.larc.pbarreto.jaes;
  * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-//import java.util.Arrays;
 
 /*********************************************************************
  * Algorithm CMAC_K^t(M)
  ********************************************************************/
-
 public final class CMAC extends MAC {
+
     private BlockCipher _E; // block cipher context
     private int block_size;
     private int block_ints;
@@ -50,16 +48,16 @@ public final class CMAC extends MAC {
         L = new int[block_ints];
         V = new int[block_ints];
         T = new byte[block_size];
-        //Arrays.fill(L, 0);
-        //Arrays.fill(V, 0);
-        //Arrays.fill(T, (byte)0);
+        fill(L, 0);
+        fill(V, 0);
+        fill(T, (byte) 0);
         rem = block_size;
         state = -1;
     }
 
     public void init() {
         // compute padding mask:
-        //Arrays.fill(L, 0);
+        fill(L, 0);
         _E.encrypt(L, L); // L = E_K(0^n)
         int c = (L[0] & 0x80000000) != 0 ? mask : 0;
         for (int b = 0; b < block_ints - 1; b++) {
@@ -67,16 +65,11 @@ public final class CMAC extends MAC {
         }
         L[block_ints - 1] = (L[block_ints - 1] << 1) ^ c; // B = 2L
         // initialize tag accumulator
-        //Arrays.fill(T, (byte)0);
+        fill(T, (byte) 0);
         rem = block_size;
         state = 0; // initialized
     }
 
-    //Eu (Eduardo) que fiz esse atalinho:    
-    public void update(byte[] M) {
-        this.update(M, M.length, true);
-    }
-    
     public void update(byte[] M, int m, boolean aad) {
         if (state != 0) {
             throw new RuntimeException("wrong order of operations");
@@ -84,42 +77,60 @@ public final class CMAC extends MAC {
         int i = 0;
         for (;;) {
             switch ((m > rem) ? rem : m) {
-            case 16: T[15] ^= M[i + 15]; // FALLTHROUGH
-            case 15: T[14] ^= M[i + 14]; // FALLTHROUGH
-            case 14: T[13] ^= M[i + 13]; // FALLTHROUGH
-            case 13: T[12] ^= M[i + 12]; // FALLTHROUGH
-            case 12: T[11] ^= M[i + 11]; // FALLTHROUGH
-            case 11: T[10] ^= M[i + 10]; // FALLTHROUGH
-            case 10: T[ 9] ^= M[i +  9]; // FALLTHROUGH
-            case  9: T[ 8] ^= M[i +  8]; // FALLTHROUGH
-            case  8: T[ 7] ^= M[i +  7]; // FALLTHROUGH
-            case  7: T[ 6] ^= M[i +  6]; // FALLTHROUGH
-            case  6: T[ 5] ^= M[i +  5]; // FALLTHROUGH
-            case  5: T[ 4] ^= M[i +  4]; // FALLTHROUGH
-            case  4: T[ 3] ^= M[i +  3]; // FALLTHROUGH
-            case  3: T[ 2] ^= M[i +  2]; // FALLTHROUGH
-            case  2: T[ 1] ^= M[i +  1]; // FALLTHROUGH
-            case  1: T[ 0] ^= M[i     ]; // FALLTHROUGH
-            case  0: break;
+                case 16:
+                    T[15] ^= M[i + 15]; // FALLTHROUGH
+                case 15:
+                    T[14] ^= M[i + 14]; // FALLTHROUGH
+                case 14:
+                    T[13] ^= M[i + 13]; // FALLTHROUGH
+                case 13:
+                    T[12] ^= M[i + 12]; // FALLTHROUGH
+                case 12:
+                    T[11] ^= M[i + 11]; // FALLTHROUGH
+                case 11:
+                    T[10] ^= M[i + 10]; // FALLTHROUGH
+                case 10:
+                    T[ 9] ^= M[i + 9]; // FALLTHROUGH
+                case 9:
+                    T[ 8] ^= M[i + 8]; // FALLTHROUGH
+                case 8:
+                    T[ 7] ^= M[i + 7]; // FALLTHROUGH
+                case 7:
+                    T[ 6] ^= M[i + 6]; // FALLTHROUGH
+                case 6:
+                    T[ 5] ^= M[i + 5]; // FALLTHROUGH
+                case 5:
+                    T[ 4] ^= M[i + 4]; // FALLTHROUGH
+                case 4:
+                    T[ 3] ^= M[i + 3]; // FALLTHROUGH
+                case 3:
+                    T[ 2] ^= M[i + 2]; // FALLTHROUGH
+                case 2:
+                    T[ 1] ^= M[i + 1]; // FALLTHROUGH
+                case 1:
+                    T[ 0] ^= M[i]; // FALLTHROUGH
+                case 0:
+                    break;
             }
             if (m <= rem) {
                 break;
             }
-            _E.byte2int(T, V); _E.encrypt(V, V); _E.int2byte(V, T); // since there is more data, no padding applies
+            _E.byte2int(T, V);
+            _E.encrypt(V, V);
+            _E.int2byte(V, T); // since there is more data, no padding applies
             // proceed to the next block:
             m -= rem;
             i += rem;
             rem = block_size;
-            //assert(m > 0);
+        //assert(m > 0);
         }
         rem -= m;
-        //assert(m == 0 || t < block_size); // m == 0 here only occurs if m == 0 from the very beginning
+    //assert(m == 0 || t < block_size); // m == 0 here only occurs if m == 0 from the very beginning
     }
 
-    //Tirei o atalhinho que tinha (tava errado?)
-    //public void update(byte[] M) {
-    //    update(M, block_size, false);
-    //}
+    public void update(byte[] M) {
+        update(M, M.length, false);
+    }
 
     public void finish(boolean aad) {
         if (state != 0) {
@@ -141,7 +152,8 @@ public final class CMAC extends MAC {
         for (int b = 0; b < block_ints; b++) {
             V[b] ^= L[b];
         }
-        _E.encrypt(V, V); _E.int2byte(V, T); // T contains the complete tag
+        _E.encrypt(V, V);
+        _E.int2byte(V, T); // T contains the complete tag
         state = 1; // CMAC tag available
     }
 
@@ -157,17 +169,28 @@ public final class CMAC extends MAC {
         }
         System.arraycopy(T, 0, tag, 0, (t < block_size) ? t : block_size);
     }
-    
+
     public void getTag(byte[] tag) {
-        this.getTag(tag, this.block_size);
+        this.getTag(tag, tag.length);
     }
 
-    public void doFinalize() {
-        //Arrays.fill(L, 0);
-        //Arrays.fill(V, 0);
-        //Arrays.fill(T, (byte)0);
+    public void finalize() {
+        fill(L, 0);
+        fill(V, 0);
+        fill(T, (byte) 0);
         rem = 0;
         state = -1;
     }
 
+    private void fill(byte[] vector, byte value) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (byte) value;
+        }
+    }
+
+    private void fill(int[] vector, int value) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = value;
+        }
+    }
 }
